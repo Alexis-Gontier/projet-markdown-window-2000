3d633b3295e37267cddbaae9f9ac6a340f336aa8
Alexis G <alexis.gontier03@gmail.com>
2024-10-19 00:25:31 +0200

    🚀 Lien navlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/page/Dashboard';
+import Home from './components/page/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <div>
+        <h1>Projet React</h1>
+        <ul>
+          <li><NavLink to="/">Accueil</NavLink></li>
+          <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+        </ul>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
+
   )
 }
 

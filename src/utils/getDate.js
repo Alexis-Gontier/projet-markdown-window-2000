d7fce45f7f9baa401e65c89585551c317605b647
gontier <alexis.gontier03@laposte.net>
2024-10-21 13:54:44 +0200

    update

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -2,9 +2,17 @@
 
 function getDate() {
     const date = new Date();
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    const options = {
+        weekday: undefined,
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+    };
 
-    const dateFR = date.toLocaleDateString("fr", options);
+    const dateFR = date.toLocaleDateString("fr-FR", options);
 
     return dateFR;
 }

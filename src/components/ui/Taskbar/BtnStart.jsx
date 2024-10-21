3e9751538fdea419bef9db0b53257135a6ff01d5
Alexis G <alexis.gontier03@gmail.com>
2024-10-21 22:47:52 +0200

    💄 BtnStart style

---
+++ src/components/ui/Taskbar/BtnStart.jsx
@@ -0,0 +1,20 @@
+import Logo from '../../../assets/images/logo-windows-2000.png'
+
+export default function BtnStart() {
+  return (
+    <button className="
+      bg-[#D7D0C8] h-full py-1 px-2 flex justify-center items-center gap-1 border-[3px] border-t-[#fff] border-r-[#808080] border-b-[#808080] border-l-[#fff] shadow-[2px_2px_0px_0px_#000]
+      focus:bg-[#c7c1ba] focus:border-t-[#808080] focus:border-r-[#fff] focus:border-b-[#fff] focus:border-l-[#808080] focus:shadow-[0px_0px_0px_0px_#000]
+      focus:translate-x-px focus:translate-y-px
+      ">
+      <img
+        className="h-full"
+        src={Logo}
+        alt="Logo Windows 2000"
+      />
+      <span className="text-base font-bold">
+        Start
+      </span>
+    </button>
+  )
+}
--- src/layouts/Taskbar.jsx
+++ src/layouts/Taskbar.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import BtnStart from '../components/ui/Taskbar/BtnStart'
 
 function Taskbar() {
   return (
-    <header className="z-100 bg-nav fixed left-0 bottom-0 w-full h-12 p-1 flex justify-between items-center">
-        <nav className="bg-red-500 w-full h-full">
-
+    <header className="z-100 bg-nav fixed left-0 bottom-0 w-full h-12 p-1 flex justify-between items-center shadow-[0px_0px_1px_0px_#1a202c]">
+        <nav className="w-full h-full">
+          <BtnStart />
         </nav>
     </header>
   )
 }
-
-Taskbar.propTypes = {}
 
 export default Taskbar

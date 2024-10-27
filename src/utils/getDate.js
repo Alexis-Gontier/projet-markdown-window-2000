a9da0c8a6e079eb4e72e4336067be246d625159d
Alexis G <alexis.gontier03@gmail.com>
2024-10-27 11:27:18 +0100

    👷 date fix

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,9 +1,7 @@
 // function pour la date
 
-export default function getDate({ options }) {
+export default function getDate(options) {
     const date = new Date();
-
     const dateFR = date.toLocaleTimeString("fr-FR", options);
-
     return dateFR;
 }

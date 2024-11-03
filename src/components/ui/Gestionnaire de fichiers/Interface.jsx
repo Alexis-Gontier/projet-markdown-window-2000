8006a99727e96263ec0211a449761860c8d648a6
Alexis G <alexis.gontier03@gmail.com>
2024-11-03 16:55:59 +0100

    gap +

--- src/components/ui/Gestionnaire de fichiers/Interface.jsx
+++ src/components/ui/Gestionnaire de fichiers/Interface.jsx
@@ -11,7 +11,7 @@ export default function Interface() {
         <Link to={"/"} className="group bg-[#D7D0C8] max-h-full px-2 flex justify-center items-center gap-1 border-2 border-t-[#fff] border-r-[#808080] border-b-[#808080] border-l-[#fff] shadow-[1px_1px_0px_1px_#000] focus:bg-[#c7c1ba] focus:border-t-[#808080] focus:border-r-[#fff] focus:border-b-[#fff] focus:border-l-[#808080] focus:shadow-[0px_0px_0px_0px_#000] focus:translate-x-px focus:translate-y-px">x</Link>
       </div>
       <div className="bg-white h-full m-1 mt-0 p-3 border-2 border-t-[#808080] border-r-[#fff] border-b-[#fff] border-l-[#808080]">
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex gap-3 flex-wrap">
           <Icon image={speaker} text={"Dashboard aaaaa"} link={"/dashboard/exemple"} style={"text-black"}/>
           <Icon image={speaker} text={"Dashboard aaaaa"} link={"/dashboard/exemple"} style={"text-black"}/>
         </div>

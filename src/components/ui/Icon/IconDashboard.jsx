81fc80221a8f5fc220ecc2c3a4939b120dc550dc
arthurGuillemin <aguillemin291@gmail.com>
2024-11-03 15:29:09 +0100

    Game

--- src/components/ui/Icon/IconDashboard.jsx
+++ src/components/ui/Icon/IconDashboard.jsx
@@ -1,12 +1,16 @@
 import Icon from "./Icon"
 
 import speaker from "../../../assets/images/speaker.png"
+import GameIcon from "../../../assets/images/GameIcon.png"
+import Explorer from "../../../assets/images/Explorer.png"
+import Home from "../../../assets/images/Home.png"
 
 export default function IconDashboard() {
   return (
     <div className="absolute top-0 left-0 flex flex-col gap-3 p-4">
-      <Icon image={speaker} text={"Home"} link={"/"}/>
-      <Icon image={speaker} text={"Dashboard"} link={"/dashboard"}/>
+      <Icon image={Home} text={"Home"} link={"/"}/>
+      <Icon image={Explorer} text={"Dashboard"} link={"/dashboard"}/>
+      <Icon image={GameIcon} text={"Plus ou Moins"} link={"/game"}/>
     </div>
   )
 }
---
+++ src/pages/PlusOuMoins.jsx
@@ -0,0 +1,12 @@
+import PlusOuMoins from '../components/api/PlusOuMoins';
+
+function Game() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <PlusOuMoins />
+    </div>
+  )
+}
+
+export default Game
+
